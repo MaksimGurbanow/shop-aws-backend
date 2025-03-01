6d86154cc42d03a330db1ed5eea33366d5dd8ee1
Maksim <maksim20051708@gmail.com>
2025-03-01 18:35:13 +0300

    feat: implement script for filling dynamoDB

---
+++ product-service/data/products.ts
@@ -0,0 +1,48 @@
+interface Product {
+  title: string;
+  price: number;
+  description: string;
+}
+
+export const products: Product[] = [
+  {
+    title: 'BMW X5',
+    price: 100000,
+    description: 'This is BMW X5',
+  },
+  {
+    title: 'Audi Q7',
+    price: 153200,
+    description: 'This is Audi Q7',
+  },
+  {
+    title: "Mazda CX-5",
+    price: 1500,
+    description: 'This is Mazda CX-5',
+  },
+  {
+    title: 'Toyota Camry',
+    price: 16000,
+    description: 'This is Toyota Camry',
+  },
+  {
+    title: 'Hundai Sonata',
+    price: 16000,
+    description: 'This is Hunday Sonata',
+  },
+  {
+    title: 'Wolkswagen Passat',
+    price: 150000,
+    description: 'This is Wolkswagen Passat',
+  },
+  {
+    title: 'Chevrolet Camaro',
+    price: 25000,
+    description: 'This is Chevrolet Camaro',
+  },
+  {
+    title: 'Ford Mustang',
+    price: 25000,
+    description: 'This is Ford Mustang',
+  }
+];
