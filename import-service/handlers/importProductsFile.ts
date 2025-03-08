2174d54effca4f06e4fccc9ff5fe43bb97edd41d
Maksim <maksim20051708@gmail.com>
2025-03-08 18:34:56 +0300

    feat: implement importProductsFile

---
+++ import-service/handlers/importProductsFile.ts
@@ -0,0 +1,37 @@
+import { S3 } from "aws-sdk";
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+const s3 = new S3({ region: "eu-west-1" });
+const BUCKET_NAME = "lambda-s3-integration-practice";
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    const filename = event.queryStringParameters?.name;
+    if (!filename) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing "name" query parameter' }),
+      };
+    }
+
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: `uploaded/${filename}`,
+      ContentType: "text/csv",
+      Expires: 120,
+    };
+
+    const signedURL = await s3.getSignedUrlPromise("putObject", params);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ signedURL }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal server error" }),
+    }
+  }
+};
---
+++ import-service/lib/import-service-stack.ts
@@ -0,0 +1,58 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import path from "node:path";
+
+export class ImportServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = s3.Bucket.fromBucketName(
+      this,
+      "ImportProductsBucketExisting",
+      "lambda-s3-integration-practice"
+    );
+
+    const environment = {
+      BUCKET_NAME: bucket.bucketName,
+    };
+
+    const layers = [
+      new lambda.LayerVersion(this, "NodeJsLayer", {
+        code: lambda.Code.fromAsset(path.join(__dirname, "../layers")),
+        compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+        description: "Dependencies layer",
+      }),
+    ];
+
+    const importProductsFile = new lambda.Function(this, "ImportProductsFile", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "handlers/importProductsFile.handler",
+      code: lambda.Code.fromAsset(path.join(__dirname, "../dist/handlers")),
+      environment,
+      layers,
+    });
+
+    bucket.grantPut(importProductsFile);
+
+    const api = new apigateway.LambdaRestApi(this, "ImportProductsFileAPI", {
+      handler: importProductsFile,
+      restApiName: "Import Products Service",
+      description: "This service allows importing products from CSV file",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+      proxy: false,
+    });
+
+    const importProducts = api.root.addResource("import");
+    importProducts.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(importProductsFile)
+    );
+  }
+}
